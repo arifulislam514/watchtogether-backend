0ac2a8d50d08a0681510066a683242e85b990238
arifulislam514 <ariful425616@gmail.com>
2026-03-16 21:33:43 +0600

    fix: install ffmpeg on Render

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install ffmpeg binary
+apt-get update && apt-get install -y ffmpeg
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
